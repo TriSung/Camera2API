db2a66741b2ba0e53ecb8879ca6c595e808d6847
gch04721 <gch0417@naver.com>
2020-09-22 04:59:16 +0900

    add AutoFitSurfaceView & SurfaceListener(Callback) - might implementation later

---
+++ Camera2Lib/app/src/main/java/com/example/camera2lib/autoFitSurfaceView/SurfaceListener.kt
@@ -0,0 +1,20 @@
+package com.example.camera2lib.autoFitSurfaceView
+
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import com.example.camera2lib.MainActivity
+
+// Implement this later
+class SurfaceListener(internal val activity : MainActivity, internal val surfaceView: AutoFitSurfaceView) : SurfaceHolder.Callback {
+    override fun surfaceCreated(holder: SurfaceHolder?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+        TODO("Not yet implemented")
+    }
+}
